d38aa574cf6d70789c4f22d10b0fdc2376bc8ca2
MRA1718 <muhammad.raihan1617@gmail.com>
2022-10-03 15:35:06 +0700

    store string words to array

--- test-array/test-array.ino
+++ test-array/test-array.ino
@@ -1,15 +1,37 @@
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);
+
+    String data = "START PUMP 3";
+    int data_len = data.length() + 1;
+    String data_array[data_len];
+    int dataCount = 0;
+    //data.toCharArray(data_array, data_len);
+
+    Serial.println(data_len);
+
+    while (data.length() > 0) {
+      int index = data.indexOf(' ');
+      if (index == -1) // No space found
+      {
+        data_array[dataCount++] = data;
+        break;
+      } else {
+        data_array[dataCount++] = data.substring(0, index);
+        data = data.substring(index+1);
+      }
+    }
+
+    Serial.println(dataCount);
+    
+    for (int i=0; i < dataCount; i++) {
+      Serial.println(data_array[i]);
+    }
+    //Serial.println(data_array[11]);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    String data = "START PUMP 3";
-    int data_len = data.length() + 1;
-    char data_array[data_len];
-    data.toCharArray(data_array, data_len);
-
-    for (
-
+    
+  
 }
