956cf2b1a62a6011003a76590ef7a8ff834be43c
MRA1718 <muhammad.raihan1617@gmail.com>
2022-10-17 13:41:13 +0700

    update testing sensor tanah

--- Arduino Test/soil-mois-test/soil-mois-test.ino
+++ Arduino Test/soil-mois-test/soil-mois-test.ino
@@ -1,16 +1,45 @@
 int output_temp = 673;
-void setup() {
-  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+
+const int power_sTanah = 9;
+const int sTanah1_pin = A0;
+const int sTanah2_pin = A1;
+int dsTanah1, dsTanah2;
+
+int sTanah(int dTanah) {
+  int res;
+  if(dTanah == 1){
+    res = analogRead(sTanah1_pin);
+    return res; 
+  } else
+  if(dTanah == 2){
+    res = analogRead(sTanah2_pin);
+    return res;
+  }
 }
+
+void setup() {
+  Serial.begin(115200); 
+  pinMode(power_sTanah, OUTPUT);
+}
+
+char txt = 0;
+
 void loop() {
-  if(output_temp > analogRead(A0)){
-    output_temp = analogRead(A0);
-    Serial.println(output_temp); 
-  }
-  else if(output_temp <= analogRead(A0)){
-    Serial.print(output_temp);
-    Serial.print("\t");
-    Serial.println(analogRead(A0));
-  }
+  /*if (Serial.available() > 0) {
+    txt = Serial.read();
+
+    if (txt == '1')  {*/
+      String rep;
+      digitalWrite(power_sTanah, HIGH);
+      //Serial.println("sensor power on");
+      dsTanah1 = sTanah(1); 
+      dsTanah2 = sTanah(2);
+      //digitalWrite(power_sTanah, LOW);
+      //Serial.println("sensor power off");
+      rep = (String) "sensor tanah 1: " + dsTanah1 + ", sensor tanah 2: " + dsTanah2;
+      Serial.println(rep);
+    //}
+  //}
+  
   delay(1000);
 }
