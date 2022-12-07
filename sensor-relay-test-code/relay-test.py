a35df45fed344c837fa3e588eefc6e490c9c2154
MRA1718 <muhammad.raihan1617@gmail.com>
2022-12-07 21:47:04 +0700

    add relay-test code

---
+++ sensor-relay-test-code/relay-test.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+pinRelay = 17
+GPIO.setup(pinRelay, GPIO.OUT)
+GPIO.output(pinRelay, GPIO.HIGH)
+time.sleep(30)
+GPIO.output(pinRelay, GPIO.LOW)
