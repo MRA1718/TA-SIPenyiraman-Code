138301331b961ad7aecb86030a9d32e033112442
MRA1718 <muhammad.raihan1617@gmail.com>
2022-12-17 16:34:43 +0700

    fix manual relay command, add manual sensor command

--- rpi4-watering-garden-code/rpi4-sensor-handler.py
+++ rpi4-watering-garden-code/rpi4-sensor-handler.py
@@ -3,25 +3,27 @@ import serial
 import time
 from datetime import datetime
 
-dict_json = ""
-
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-ser.reset_input_buffer()
-#i=1
-while dict_json == "":
-#while i <= 5:
-    ser.write("collectData\n".encode('utf-8'))
-    data = ser.readline().decode("utf-8")
-    dtime = datetime.now()
-    try:
-        dict_json = json.loads(data)
-        print(dict_json, dtime)
-    except json.JSONDecodeError as e:
-        print("JSON:", e, dtime)
-    #i += 1
-    #time.sleep(1)
+def dataFetch():
+    dict_json = ""
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.reset_input_buffer()
+    #i=1
+    while dict_json == "":
+    #while i <= 5:
+        ser.write("collectData\n".encode('utf-8'))
+        data = ser.readline().decode("utf-8")
+        dtime = datetime.now()
+        try:
+            dict_json = json.loads(data)
+            return(dict_json, dtime)
+        except json.JSONDecodeError as e:
+            return("JSON:", e, dtime)
+        #i += 1
+        #time.sleep(1)
+"""
 print(dict_json.get("soil1"))
 print(dict_json.get("soil2"))
 print(round(dict_json.get("temp"), 2))
 print(round(dict_json.get("humidity"), 2))
 print(dict_json.get("light"))
+"""
--- rpi4-watering-garden-code/rpi4-telegram-handler.py
+++ rpi4-watering-garden-code/rpi4-telegram-handler.py
@@ -1,25 +1,53 @@
 import time
-import random
-import datetime
-import telepot
+import threading
+import telepot 
 import RPi.GPIO as GPIO
+import json
+import serial
+from datetime import datetime
 from telepot.loop import MessageLoop
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 pinRelay = 17
 GPIO.setup(pinRelay, GPIO.OUT)
-pumpFlag = 0
-"""
-def relay():
+pumpFlag = 0 #act as status of relay
+
+def dataFetch():
+    dict_json = ""
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.reset_input_buffer()
+    #i=1
+    while dict_json == "":
+    #while i <= 5:
+        ser.write("collectData\n".encode('utf-8'))
+        data = ser.readline().decode("utf-8")
+        dtime = datetime.now()
+        try:
+            dict_json = json.loads(data)
+            return(dict_json)
+        except json.JSONDecodeError as e:
+            ("JSON:", e)
+        #i += 1
+        #time.sleep(1)
+
+#Function for turning on pump relay
+def relayOn():
     global pumpFlag
     
     GPIO.output(pinRelay, GPIO.HIGH)
     pumpFlag = 1
-    time.sleep(180)
+
+#Function for turning off pump relay
+def relayOff(chat_id):
+    global pumpFlag
+
     GPIO.output(pinRelay, GPIO.LOW)
     pumpFlag = 0
-"""
+    bot.sendMessage(chat_id, 'Mematikan pompa, penyiraman selesai')
+
+#Function for telegram message handling
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
@@ -30,16 +58,19 @@ def handle(msg):
     if command == '/pompa':
         if pumpFlag == 0:
             bot.sendMessage(chat_id, 'Menyalakan pompa selama 3 menit.')
-            print('1st msg')
-            GPIO.output(pinRelay, GPIO.HIGH)
-            pumpFlag = 1
-            time.sleep(180)
-            GPIO.output(pinRelay, GPIO.LOW)
-            pumpFlag = 0
-            bot.sendMessage(chat_id, 'Mematikan pompa, penyiraman selesai')
-        else:
+            relayOn()
+            timer= threading.Timer(180.0, relayOff, args=[chat_id])
+            timer.start()
+        elif pumpFlag == 1:
             bot.sendMessage(chat_id, 'Pompa sedang menyala')
-            print('2nd msg')
+    elif command == '/sensor':
+        sData = dataFetch()
+        sMsg = "Sensor Tanah 1: " + str(sData.get("soil1")) + "\nSensor Tanah 2: " + str(sData.get("soil2"))
+        print(sMsg)
+        bot.sendMessage(chat_id, sMsg)
+    else:
+        bot.sendMessage(chat_id, "List Command: /pompa")
+            
 
 bot = telepot.Bot('5725314127:AAHxruba6S34B0He6rQ8vVRlJ6IZ_0ucgQ4')
 
